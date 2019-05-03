2c29d066833b9e26f3569e304c04f1d2edecad99
SergeVassal <41280771+SergeVassal@users.noreply.github.com>
2019-05-03 13:29:48 +0200

    Work 03.05.

---
+++ HoverCar2019/Assets/Scripts/HoverAudio.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverAudio : MonoBehaviour
+{
+	[SerializeField] private AudioSource jetSound;
+
+	private float jetPitch;
+	private const float LowPitch=.1f;
+	private const float HighPitch=2.0f;
+	private const float SpeedToRevs=.01f;
+	private Vector3 myVelocity;
+	private Rigidbody carRigidbody;
+
+
+
+	void Awake () 
+	{		
+		carRigidbody = GetComponent<Rigidbody> ();	
+	}
+
+	void FixedUpdate()
+    {
+		myVelocity = carRigidbody.velocity;
+		float forwardSpeed = carRigidbody.velocity.z;
+		float engineRevs = Mathf.Abs (forwardSpeed) * SpeedToRevs;
+		jetSound.pitch = Mathf.Clamp (engineRevs, LowPitch, HighPitch);
+	}
+}
---
+++ HoverCar2019/Assets/Standard Assets/Vehicles/Car/Scripts/BrakeLight.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UnitySampleAssets.Vehicles.Car
+{
+    public class BrakeLight : MonoBehaviour
+    {
+        public CarController car; // reference to the car controller, must be dragged in inspector
+
+
+        
+    }
+}
