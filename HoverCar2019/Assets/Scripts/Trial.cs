268e20a97d5733e6f8f5a9e2928ad3a273de5e37
SergeVassal <41280771+SergeVassal@users.noreply.github.com>
2019-05-23 08:05:43 +0200

    Work 04.05.

---
+++ HoverCar2019/Assets/Scripts/Trial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trial : MonoBehaviour 
+{
+    private void Start()
+    {
+        float source = 130.1234567f;
+        float f = (float)(source - Math.Truncate(source));
+        int i = (int)(f * 1000);
+        Debug.Log(i);
+    }
+}
